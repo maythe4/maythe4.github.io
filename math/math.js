a3ad23f2c3dbcb92392c1b73f77eee006cfbb550
Kaloyan Zaverdzhiev <jan20161116@gmail.com>
2020-10-09 08:21:26 +0200

    math delete history

--- math/math.js
+++ math/math.js
@@ -41,9 +41,23 @@ function showQuestion() {
 }
 
 function onButtonClick(event) {
-    event.target.classList = 'button-clicked';
-    setTimeout(function() { event.target.classList = 'button'; }, 300);
-    performAction(event.target.getAttribute('action'));
+    let target = findTargetWithAction(event.target);
+    if (target)
+    {
+        target.classList = 'button-clicked';
+        setTimeout(function() { target.classList = 'button'; }, 300);
+        performAction(target.getAttribute('action'));
+    }
+}
+
+function findTargetWithAction(target) {
+    if (target) {
+        if (target.getAttribute('action')) {
+            return target;
+        }
+        return findTargetWithAction(target.parentElement);
+    }
+    return target;
 }
 
 function onKeyDown(event) {
@@ -115,4 +129,28 @@ function putQuestionInHistory() {
     innerHtml += '</ol>';
     document.getElementById('history').innerHTML = innerHtml;
     document.getElementById('mistakes').innerHTML = (new Date()).toLocaleDateString('en-US', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'}) + ', mistakes: ' + mistakes;
+    createHistoryButtons();
+}
+
+function createHistoryButtons() {
+    let container = document.getElementById('history-buttons');
+    if (!container.innerHTML) {
+        let div = document.createElement('div');
+        container.appendChild(div);
+        div.classList = 'button';
+        div.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
+        div.addEventListener('click', function() { 
+            deleteHistory();
+            lastResult = 0;
+            randomNumbers = [];
+            createQuestion();
+        });
+    } 
+}
+
+function deleteHistory() {
+    history = [];
+    document.getElementById('mistakes').innerHTML = '';
+    document.getElementById('history').innerHTML = '';
+    document.getElementById('history-buttons').innerHTML = '';
 }
