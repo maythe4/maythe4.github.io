0397f6f7f0d877d523c4eb65b9a31940d4c47641
Kaloyan Zaverdzhiev <jan20161116@gmail.com>
2020-10-10 05:03:54 +0200

    math saves state

--- math/math.js
+++ math/math.js
@@ -1,31 +1,28 @@
 document.addEventListener('keydown', onKeyDown);
 document.getElementById('buttons').addEventListener('click', onButtonClick);
 
-let lastResult = 0;
-let question = {};
-let randomNumbers = [];
-let history = [];
-
+let state = {};
+loadState();
 createQuestion();
 
 function createQuestion() {
-    question = {};
-    question.number1 = lastResult === 0 ? getRandomNumber() : lastResult;
-    question.number2 = getRandomNumber();
-    question.operation = question.number2 >= question.number1 ? 0 : Math.floor(Math.random() * 2); // 0 = +, 1 = -
-    question.expectedAnswer = question.operation === 0 ? question.number1 + question.number2 : question.number1 - question.number2;
-    question.answer = '';
+    state.question = {};
+    state.question.number1 = state.lastResult === 0 ? getRandomNumber() : state.lastResult;
+    state.question.number2 = getRandomNumber();
+    state.question.operation = state.question.number2 >= state.question.number1 ? 0 : Math.floor(Math.random() * 2); // 0 = +, 1 = -
+    state.question.expectedAnswer = state.question.operation === 0 ? state.question.number1 + state.question.number2 : state.question.number1 - state.question.number2;
+    state.question.answer = '';
     showQuestion();
 }
 
 function getRandomNumber() {
-    if (randomNumbers.length < 1)  {
-        let start = (Math.floor((history.length + 1) / 10) * 10) + 1;
-        for (let i = start; i < start + 9; i++) randomNumbers.push(i);
-        shuffleArray(randomNumbers);
-        randomNumbers.push(randomNumbers[0]);
+    if (state.randomNumbers.length < 1)  {
+        let start = (Math.floor((state.history.length + 1) / 10) * 10) + 1;
+        for (let i = start; i < start + 9; i++) state.randomNumbers.push(i);
+        shuffleArray(state.randomNumbers);
+        state.randomNumbers.push(state.randomNumbers[0]);
     }
-    return randomNumbers.pop();
+    return state.randomNumbers.pop();
 }
 
 function shuffleArray(array) {
@@ -36,8 +33,8 @@ function shuffleArray(array) {
 }
 
 function showQuestion() {
-    question.text = question.number1 + (question.operation === 0 ? ' + ' : ' - ') + question.number2 + ' = ';
-    document.getElementById('question').innerHTML = question.text + question.answer;
+    state.question.text = state.question.number1 + (state.question.operation === 0 ? ' + ' : ' - ') + state.question.number2 + ' = ';
+    document.getElementById('question').innerHTML = state.question.text + state.question.answer;
 }
 
 function onButtonClick(event) {
@@ -52,10 +49,7 @@ function onButtonClick(event) {
 
 function findTargetWithAction(target) {
     if (target) {
-        if (target.getAttribute('action')) {
-            return target;
-        }
-        return findTargetWithAction(target.parentElement);
+        return target.getAttribute('action') ? target : findTargetWithAction(target.parentElement);
     }
     return target;
 }
@@ -99,36 +93,42 @@ function performAction(action) {
 }
 
 function addToAnswer(value) {
-    question.answer += value;
+    state.question.answer += value;
     showQuestion();
 }
 
 function deleteFromAnswer() {
-    question.answer = question.answer.slice(0, -1);
+    state.question.answer = state.question.answer.slice(0, -1);
     showQuestion();
 }
 
 function checkAnswer() {
-    lastResult = question.expectedAnswer;
+    state.lastResult = state.question.expectedAnswer;
     putQuestionInHistory();
+    showHistory();
     createQuestion();
 }
 
 function putQuestionInHistory() {
-    history.unshift(question);
+    state.history.unshift(state.question);
+    state.historyDate = (new Date()).toLocaleDateString('en-US', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'});
+    saveState();
+}
+
+function showHistory() {
     let mistakes = 0;
     let innerHtml = '<ol reversed>';
-    for (let i = 0; i < history.length; i++) {
-        if (history[i].expectedAnswer === parseInt(history[i].answer)) {
-            innerHtml += '<li>' + history[i].text + history[i].answer + '</li>';
+    for (let i = 0; i < state.history.length; i++) {
+        if (state.history[i].expectedAnswer === parseInt(state.history[i].answer)) {
+            innerHtml += '<li>' + state.history[i].text + state.history[i].answer + '</li>';
         } else {
-            innerHtml += '<li class="wrong">' + history[i].text + history[i].expectedAnswer + ' (&ne; ' + history[i].answer + ')</li>';
+            innerHtml += '<li class="wrong">' + state.history[i].text + state.history[i].expectedAnswer + ' (&ne; ' + state.history[i].answer + ')</li>';
             mistakes++;
         }
     }
     innerHtml += '</ol>';
     document.getElementById('history').innerHTML = innerHtml;
-    document.getElementById('mistakes').innerHTML = (new Date()).toLocaleDateString('en-US', {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit'}) + ', mistakes: ' + mistakes;
+    document.getElementById('mistakes').innerHTML = state.historyDate + ', mistakes: ' + mistakes;
     createHistoryButtons();
 }
 
@@ -140,16 +140,36 @@ function createHistoryButtons() {
         div.classList = 'button';
         div.innerHTML = '<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
         div.addEventListener('click', function() { 
-            deleteHistory();
-            lastResult = 0;
-            randomNumbers = [];
+            resetState();
             createQuestion();
         });
     } 
 }
 
-function deleteHistory() {
-    history = [];
+function saveState() {
+    if (state) localStorage.setItem('math', JSON.stringify(state));
+    else localStorage.removeItem('math');
+}
+
+function loadState() {
+    let savedState = localStorage.getItem('math');
+    if (savedState) {
+        state = JSON.parse(savedState);
+        createQuestion();
+        showHistory();
+    }
+    else {
+        resetState();
+    }
+}
+
+function resetState() {
+    state = {
+        lastResult: 0,
+        question: {},
+        randomNumbers: [],
+        history: []
+    }
     document.getElementById('mistakes').innerHTML = '';
     document.getElementById('history').innerHTML = '';
     document.getElementById('history-buttons').innerHTML = '';
